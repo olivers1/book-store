ce9de9b4a59915268ce6ae825ece8c2290413c9c
olivers1 <oliver.staberg@gmail.com>
2019-08-28 20:58:24 +0200

    crash

--- book store/book store/Page.h
+++ book store/book store/Page.h
@@ -9,20 +9,24 @@ class Page
 {
 private:
 	Page* m_parent;		// pointer to parent page
-	vector<Page>* m_child;	// pointer to vector that contains the children pages of current page
+	//vector<Page>* m_child;	// pointer to vector that contains the children pages of current page
 	vector<Page> m_vecChildren;		// vector containing all children of current page
 
 	string m_pageName;		// name of the page
 public:
-	Page(Page* parent, string pageName) : m_parent(parent), m_pageName(pageName), m_child(&m_vecChildren) {}
+	Page(string pageName) : m_pageName(pageName) {}
 
-	void AddChild(Page&);
+	Page(Page* parent, string pageName) : m_parent(parent), m_pageName(pageName)/*, m_child(&m_vecChildren)*/ {}
+
+	void SetChild(Page&);
+
+	void SetParent(Page&);
+
+	//Page& GetParent();	// GETS THE NULLPTR AT FIRST TIME, MIGHT BE REASON WHY IT CRASHES?
 
 	const string& GetPageName();
 
 	vector<Page>& GetChildVec();
-
-	//const string& GetChildName();
 
 	Page& GetChild(int&);
 
