d262b4c60c751af947ac40358fb7e9253a070fb7
olivers1 <oliver.staberg@gmail.com>
2019-08-30 16:06:35 +0200

    child vector not updating after 2:n sub menu

--- book store/book store/Page.h
+++ book store/book store/Page.h
@@ -9,26 +9,28 @@ class Page
 {
 private:
 	Page* m_parent;		// pointer to parent page
-	//vector<Page>* m_child;	// pointer to vector that contains the children pages of current page
+	vector<Page>* m_child;	// pointer to vector that contains the children pages of current page
 	vector<Page> m_vecChildren;		// vector containing all children of current page
 
 	string m_pageName;		// name of the page
 public:
 	Page(string pageName) : m_pageName(pageName) {}
 
-	Page(Page* parent, string pageName) : m_parent(parent), m_pageName(pageName)/*, m_child(&m_vecChildren)*/ {}
+	Page(Page* parent, string pageName) : m_parent(parent), m_pageName(pageName) {}
+
+	Page(Page* parent, vector<Page>* , string pageName) : m_parent(parent), m_pageName(pageName), m_child(&m_vecChildren) {}
 
 	void SetChild(Page&);
 
 	vector<Page>& GetChildVec();
+
+	Page& GetChild(int&);
 
 	void SetParent(Page&);
 
 	Page& GetParent();
 
 	const string& GetPageName();
-
-	Page& GetChild(int&);
 
 	const int& GetChildSize();
 };
