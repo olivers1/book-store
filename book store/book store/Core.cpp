d440e258fbf841ba617a003f4ffee882eb6ca33b
olivers1 <oliver.staberg@gmail.com>
2019-09-02 16:53:10 +0200

    read access violation

--- book store/book store/Core.cpp
+++ book store/book store/Core.cpp
@@ -5,62 +5,75 @@ using namespace std;
 
 void Core::Initialize()
 {
-	Page* dummyPage = new Page(nullptr, "dummyPage");	// dummy page to avoid error when parent pointer is pointing to 'nullptr'
-	Page* mainMenu = new Page(dummyPage, "Main Menu");
+	Page* dummyPage = new Page("dummyPage", nullptr);	// dummy page to avoid error when parent pointer is pointing to 'nullptr'
+	Page* mainMenu = new Page("Main Menu", dummyPage);
+
 	dummyPage->SetChild(*mainMenu);
 
 	m_page = mainMenu;		// set mainMenu as default page
-	m_parent = &(mainMenu->GetParent());	// set pointer to point at mainMenu's parent
-	m_children = &(m_page->GetChildVec());	// set pointer to point at mainMenu's child vector containing all its children pages
 
-	// add book
-	Page* addBook = new Page(mainMenu, "Add Book");		// creating page object on the heap and assigning parent pointer to its member variable
+	Page* addBook = new Page("Add Book", mainMenu);
+	Page* setTitle = new Page("Add Book", addBook);
+	Page* setAuthor = new Page("Set Author", setTitle);
+	Page* setYear = new Page("Set Year", setAuthor);
+	Page* setPrice = new Page("Set Price", setYear);
+	Page* setStock = new Page("Set Stock", setPrice);
 
-	Page* setTitle = new Page(addBook, "Set Title");		// create page without assigning a parent to it
-
-	Page* setAuthor = new Page(setTitle, "Set Author");
-
-	Page* setYear = new Page(setAuthor, "Set Year");
-
-	Page* setPrice = new Page(setYear, "Set Price");
-
-	Page* setStock = new Page(setPrice, "Set Stock");
-
-	// find book
-	Page* findBook = new Page(mainMenu, "Search Book");
-
-	Page* titleFind = new Page(findBook, "Search by Title");
-	
-	Page* authorFind = new Page(titleFind, "Search by Author");
-
-	Page* yearFind = new Page(authorFind, "Search by Publication Year");
-
-	Page* priceFind = new Page(yearFind, "Search by Price");
-
-	Page* stockFind = new Page(priceFind, "Search by no. Items in Stock");
-
+	mainMenu->SetChild(*addBook);
 	addBook->SetChild(*setTitle);
-	setTitle->SetChild(*setAuthor);
-	setAuthor->SetChild(*setYear);
+	setTitle->SetChild(*setYear);
+	setYear->SetChild(*setPrice);
+	setPrice->SetChild(*setStock);
+	setStock->SetChild(*mainMenu);
 
-	findBook->SetChild(*titleFind);
-	findBook->SetChild(*authorFind);
-	findBook->SetChild(*yearFind);
-	findBook->SetChild(*priceFind);
-	findBook->SetChild(*stockFind);
+	//// add book
+	//Page* addBook = new Page(mainMenu, "Add Book");		// creating page object on the heap and assigning parent pointer to its member variable
 
-	// sell book
-	Page* sellBook = new Page(mainMenu, "Sell Book");
-	sellBook->SetChild(*mainMenu);
+	//Page* setTitle = new Page(addBook, "Set Title");		// create page without assigning a parent to it
 
-	// edit book
-	Page* editBook = new Page(mainMenu, "Edit Book");
+	//Page* setAuthor = new Page(setTitle, "Set Author");
 
-	// mainMenu
-	mainMenu->SetChild(*addBook);		// adding children pages to each page (vector member variable of each page/object)
-	mainMenu->SetChild(*findBook);
-	mainMenu->SetChild(*sellBook);
-	mainMenu->SetChild(*editBook);
+	//Page* setYear = new Page(setAuthor, "Set Year");
+
+	//Page* setPrice = new Page(setYear, "Set Price");
+
+	//Page* setStock = new Page(setPrice, "Set Stock");
+
+	//// find book
+	//Page* findBook = new Page(mainMenu, "Search Book");
+
+	//Page* titleFind = new Page(findBook, "Search by Title");
+	//
+	//Page* authorFind = new Page(titleFind, "Search by Author");
+
+	//Page* yearFind = new Page(authorFind, "Search by Publication Year");
+
+	//Page* priceFind = new Page(yearFind, "Search by Price");
+
+	//Page* stockFind = new Page(priceFind, "Search by no. Items in Stock");
+
+	//addBook->SetChild(*setTitle);
+	//setTitle->SetChild(*setAuthor);
+	//setAuthor->SetChild(*setYear);
+
+	//findBook->SetChild(*titleFind);
+	//findBook->SetChild(*authorFind);
+	//findBook->SetChild(*yearFind);
+	//findBook->SetChild(*priceFind);
+	//findBook->SetChild(*stockFind);
+
+	//// sell book
+	//Page* sellBook = new Page(mainMenu, "Sell Book");
+	//sellBook->SetChild(*mainMenu);
+
+	//// edit book
+	//Page* editBook = new Page(mainMenu, "Edit Book");
+
+	//// mainMenu
+	//mainMenu->SetChild(*addBook);		// adding children pages to each page (vector member variable of each page/object)
+	//mainMenu->SetChild(*findBook);
+	//mainMenu->SetChild(*sellBook);
+	//mainMenu->SetChild(*editBook);
 }
 
 void Core::Execute()
@@ -75,10 +88,14 @@ void Core::Execute()
 
 void Core::ShowMenu()
 {
-	cout << "\nParent: " << m_parent->GetPageName();	//--Hamid: m_page -> GetParent() -> GetPageName()
+	cout << "\nParents: ";
+	for (Page parent : m_page->GetParentVec())
+	{
+		cout << parent.GetPageName() << ", ";
+	}
 	cout << "\nPage: " << m_page->GetPageName();
 	cout << "\nChildren: ";
-	for (Page child : *m_children)
+	for (Page child : m_page->GetChildVec())
 	{
 		cout << child.GetPageName() << ", ";
 	}
@@ -93,10 +110,6 @@ void Core::ReadInput()
 
 void Core::UpdateState()
 {
-	//	Hamid: You just need to update the m_page, then you can update its parent and child from m_page's methods
-
 	m_page = &(m_page->GetChild(m_input));	// update page child pointer to point at selected child menu
-	m_parent = &(m_page->GetParent());	// update parent pointer to point at the parent of current page
-	m_children = &(m_page->GetChildVec());	// update child vector pointer to point at children pages of new parent
 }
 
